42556a8ef0d4f4b44837d9df45e81973b3f6cb6d
Anastasiia Smakula <anastasia.smakula@gmail.com>
2025-05-29 14:41:28 -0400

    initial commit

---
+++ src/Body.tsx
@@ -0,0 +1,6 @@
+import "./Body.css"
+const Body = () => {
+    return <div> Thanks </div>
+}
+
+export default Body;
---
+++ src/Top.tsx
@@ -0,0 +1,7 @@
+import "./Top.css"
+
+const Top = () => {
+    return <div className="top-container">Hello!</div>
+}
+
+export default Top;
