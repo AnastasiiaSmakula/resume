bb2612a65c2760f693f464d644c66a811c8d01f1
Anastasiia Smakula <anastasia.smakula@gmail.com>
2025-06-10 13:22:27 -0400

    fix cursor on action button

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,11 +3,17 @@ const Header = () => {
     <header className="header">
       <h2>Anastasiia Smakula</h2>
       <div>
-        <a href="#about">About</a>
-        <a href="#education">School</a>
-        <a href="#work">Work</a>
+        <a href="#about" className="header-link">
+          About
+        </a>
+        <a href="#education" className="header-link">
+          School
+        </a>
+        <a href="#work" className="header-link">
+          Work
+        </a>
         <a href="mailto:anastasia.smakula@gmail.com" className="action-button">
-          <button>Get in touch!</button>
+          Get in touch!
         </a>
       </div>
     </header>
